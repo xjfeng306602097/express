c8eb226385251c37e6a7b4eae7c66e1362cda4d9
xjfeng306602097 <306602097@qq.com>
2017-04-08 21:20:24 +0800

    Express Project

--- src/main/java/com/epress/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.epress.dao;
-
-/**
- * Created by wshibiao on 2017/4/5.
- */
-public interface UserDao {
-}
--- src/main/java/com/epress/service/ExpressService.java
+++
@@ -1,5 +0,0 @@
-package com.epress.service;
-
-public interface ExpressService {
-	
-}
