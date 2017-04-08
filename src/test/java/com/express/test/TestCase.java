b4a388c16c1410f9828d17e775a5a0c51bb320a7
xjfeng306602097 <306602097@qq.com>
2017-04-08 22:02:16 +0800

    测试类模板

---
+++ src/test/java/com/express/test/TestCase.java
@@ -0,0 +1,15 @@
+package com.express.test;
+
+import org.junit.Test;
+
+public class TestCase extends BaseTestCase {
+	
+	/**
+	 * 继承BaseTestCase即可
+	 */
+	@Test
+	public void test() {
+		System.out.println("Success");
+	}
+
+}
