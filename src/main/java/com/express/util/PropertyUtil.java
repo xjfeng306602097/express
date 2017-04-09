36c4fc4af3c9ff0640c7b0a1146bf0f4260d480f
xjfeng306602097 <306602097@qq.com>
2017-04-09 11:43:36 +0800

    读取Properties文件获取盐值，现在没有头绪

---
+++ src/main/java/com/express/util/PropertyUtil.java
@@ -0,0 +1,14 @@
+package com.express.util;
+
+public class PropertyUtil {
+	
+	/**
+	 * 读取工程目录下的properties文件路径，现在还没头绪
+	 * @param key
+	 * @return
+	 */
+	public static String getProperty(String key) {
+		return null;
+	}
+	
+}
