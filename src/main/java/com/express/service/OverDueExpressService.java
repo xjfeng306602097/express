8fa7f9ed9a2f0d30a5a2a99ea87a5b3bde310864
wshibiao <790207553@qq.com>
2017-04-09 22:41:06 +0800

    获取隔日货柜全部快件。

--- src/main/java/com/express/service/OverDueExpressService.java
+++ src/main/java/com/express/service/OverDueExpressService.java
@@ -1,5 +1,6 @@
 package com.express.service;
 
+import com.express.model.Express;
 import com.express.model.OverDueExpress;
 
 import java.util.List;
@@ -15,12 +16,12 @@ public interface OverDueExpressService {
      * 获取所有隔日快递的收件人联系方式
      * @return
      */
-    public List<String> getContactsWithOverDue();
+    public List<Express> getExpressWithOverDue();
     
     /**
      * 根据参数获取隔日快递的收件人联系方式
      * @param overDueExpress
-     * @return
+     * @returng
      */
     public List<OverDueExpress> queryShelfListByParams(OverDueExpress overDueExpress);
     
