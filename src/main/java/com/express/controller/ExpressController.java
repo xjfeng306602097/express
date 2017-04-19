6cccd158158bd3815768abcb28d69a778c8af081
xjfeng306602097 <306602097@qq.com>
2017-04-19 18:12:31 +0800

    添加controller处理方法

--- src/main/java/com/express/controller/ExpressController.java
+++ src/main/java/com/express/controller/ExpressController.java
@@ -9,6 +9,7 @@ import com.express.service.ExpressShelfService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,34 +28,44 @@ public class ExpressController {
 	ExpressShelfService expressShelfService;
 	@Resource
 	ExpressShelfDao expressShelfDao;
+
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String toIndex(Model model) {
 		return "express/express";
 	}
+
 	@ResponseBody
 	@RequestMapping(value = "/getParam", method = RequestMethod.GET)
-	public
-	List<Express> getQueryParam(HttpServletRequest request, HttpServletResponse response) {
+	public List<Express> getQueryParam(HttpServletRequest request, HttpServletResponse response) {
 
-		String contact=request.getParameter("contact");
-		String expressNo=request.getParameter("expressNo");
-		String arriveDate=request.getParameter("arriveDate");
-		List<Express> expresses=expressService.queryExpressInfo(contact,expressNo);
+		String contact = request.getParameter("contact");
+		String expressNo = request.getParameter("expressNo");
+		String arriveDate = request.getParameter("arriveDate");
+		List<Express> expresses = expressService.queryExpressInfo(contact, expressNo);
 		return expresses;
 	}
 
 	@ResponseBody
 	@RequestMapping(value = "/getShelf", method = RequestMethod.GET)
-	public
-	Object getExpressShelf(HttpServletRequest request,HttpServletResponse response) {
+	public Object getExpressShelf(HttpServletRequest request, HttpServletResponse response) {
 		JSONObject jsonObject = new JSONObject();
-		Long id=Long.parseLong(request.getParameter("id"));
-		Express express=expressService.getExpressInfoById(id);
-		ExpressShelf shelf=new ExpressShelf();
+		Long id = Long.parseLong(request.getParameter("id"));
+		Express express = expressService.getExpressInfoById(id);
+		ExpressShelf shelf = new ExpressShelf();
 		shelf.setExpress(express);
-		shelf=expressShelfDao.queryShelfByParams(shelf);
+		shelf = expressShelfDao.queryShelfByParams(shelf);
 		return shelf;
 	}
 
-}
+	@RequestMapping(value = "/vegas", method = RequestMethod.GET)
+	public String toVegasIndex(Model model) {
+		return "express/index";
+	}
 
+	@RequestMapping(value = "/getExpressList", method = RequestMethod.POST)
+	@ResponseBody
+	public List<Express> getExpressList(@RequestBody Express express) {
+		List<Express> expressList = expressService.queryExpressInfo(express.getContact(), express.getExpressNo());
+		return expressList;
+	}
+}
