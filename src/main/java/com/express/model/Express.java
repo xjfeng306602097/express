40fb4ed4114f1d239c626c7f55ba08dfa3b103ef
xjfeng306602097 <306602097@qq.com>
2017-04-24 18:46:33 +0800

    添加Express字段email_address

--- src/main/java/com/express/model/Express.java
+++ src/main/java/com/express/model/Express.java
@@ -7,159 +7,171 @@ import java.util.Date;
  * Created by wshibiao on 2017/4/5.
  */
 public class Express implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * id
-     */
-    private Long id;
-    /**
-     * 快递单号
-     */
-    private String expressNo;
-    /**
-     * 寄件时间
-     */
-    private Date fromDate;
-    /**
-     * 到达时间
-     */
-    private Date arriveDate;
-    /**
-     * 取件时间
-     */
-    private Date receiveDate;
-    /**
-     * 联系方式
-     */
-    private String contact;
-    /**
-     * 寄件源地址
-     */
-    private String addressSource;
-    /**
-     * 到达地址
-     */
-    private String addressDest;
-    /**
-     * 收件人
-     */
-    private String consignee;
-    /**
-     * 快递公司
-     */
-    private String company;
-    /**
-     *验证码
-     */
-    private String verificationCode;
-    /**
-     * 取件状态
-     */
-    private String status;
+	/**
+	 * id
+	 */
+	private Long id;
+	/**
+	 * 快递单号
+	 */
+	private String expressNo;
+	/**
+	 * 寄件时间
+	 */
+	private Date fromDate;
+	/**
+	 * 到达时间
+	 */
+	private Date arriveDate;
+	/**
+	 * 取件时间
+	 */
+	private Date receiveDate;
+	/**
+	 * 联系方式
+	 */
+	private String contact;
+	/**
+	 * 寄件源地址
+	 */
+	private String addressSource;
+	/**
+	 * 到达地址
+	 */
+	private String addressDest;
+	/**
+	 * 收件人
+	 */
+	private String consignee;
+	/**
+	 * 快递公司
+	 */
+	private String company;
+	/**
+	 * 验证码
+	 */
+	private String verificationCode;
+	/**
+	 * 取件状态
+	 */
+	private String status;
+	/**
+	 * 邮箱地址
+	 */
+	private String emailAddress;
 
-    public void setReceiveDate(Date receiveDate) {
-        this.receiveDate = receiveDate;
-    }
+	public void setReceiveDate(Date receiveDate) {
+		this.receiveDate = receiveDate;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-    public String getExpressNo() {
-        return expressNo;
-    }
+	public String getExpressNo() {
+		return expressNo;
+	}
 
-    public void setExpressNo(String expressNo) {
-        this.expressNo = expressNo;
-    }
+	public void setExpressNo(String expressNo) {
+		this.expressNo = expressNo;
+	}
 
-    public String getContact() {
-        return contact;
-    }
+	public String getContact() {
+		return contact;
+	}
 
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
 
-    public String getAddressSource() {
-        return addressSource;
-    }
+	public String getAddressSource() {
+		return addressSource;
+	}
 
-    public void setAddressSource(String addressSource) {
-        this.addressSource = addressSource;
-    }
+	public void setAddressSource(String addressSource) {
+		this.addressSource = addressSource;
+	}
 
-    public String getAddressDest() {
-        return addressDest;
-    }
+	public String getAddressDest() {
+		return addressDest;
+	}
 
-    public void setAddressDest(String addressDest) {
-        this.addressDest = addressDest;
-    }
+	public void setAddressDest(String addressDest) {
+		this.addressDest = addressDest;
+	}
 
-    public String getConsignee() {
-        return consignee;
-    }
+	public String getConsignee() {
+		return consignee;
+	}
 
-    public void setConsignee(String consignee) {
-        this.consignee = consignee;
-    }
+	public void setConsignee(String consignee) {
+		this.consignee = consignee;
+	}
 
-    public String getCompany() {
-        return company;
-    }
+	public String getCompany() {
+		return company;
+	}
 
-    public void setCompany(String company) {
-        this.company = company;
-    }
+	public void setCompany(String company) {
+		this.company = company;
+	}
 
-    public String getVerificationCode() {
-        return verificationCode;
-    }
+	public String getVerificationCode() {
+		return verificationCode;
+	}
 
-    public void setVerificationCode(String verificationCode) {
-        this.verificationCode = verificationCode;
-    }
+	public void setVerificationCode(String verificationCode) {
+		this.verificationCode = verificationCode;
+	}
 
-    public Date getFromDate() {
-        return fromDate;
-    }
+	public Date getFromDate() {
+		return fromDate;
+	}
 
-    public void setFromDate(Date fromDate) {
-        this.fromDate = fromDate;
-    }
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
 
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
+	}
 
-    public Date getArriveDate() {
-        return arriveDate;
-    }
+	public Date getArriveDate() {
+		return arriveDate;
+	}
 
-    public void setArriveDate(Date arriveDate) {
-        this.arriveDate = arriveDate;
-    }
+	public void setArriveDate(Date arriveDate) {
+		this.arriveDate = arriveDate;
+	}
 
-    public Date getReceiveDate() {
-        return receiveDate;
-    }
+	public Date getReceiveDate() {
+		return receiveDate;
+	}
 
-    public void setReciveDate(Date receiveDate) {
-        this.receiveDate = receiveDate;
-    }
+	public void setReciveDate(Date receiveDate) {
+		this.receiveDate = receiveDate;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
 
 }
