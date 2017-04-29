9eea5ecaa74249c4fb84271d52f19ecff597b543
wshibiao <790207553@qq.com>
2017-04-29 23:13:59 +0800

    去除无用的方法

--- src/main/java/com/express/controller/ExpressController.java
+++ src/main/java/com/express/controller/ExpressController.java
@@ -1,13 +1,11 @@
 package com.express.controller;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.alibaba.fastjson.JSONObject;
+import com.express.model.Express;
+import com.express.model.ExpressHistory;
+import com.express.model.ExpressShelf;
+import com.express.model.OverDueExpress;
+import com.express.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,17 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSONObject;
-import com.express.dao.ExpressShelfDao;
-import com.express.model.Express;
-import com.express.model.ExpressHistory;
-import com.express.model.ExpressShelf;
-import com.express.model.OverDueExpress;
-import com.express.service.ExpressHistoryService;
-import com.express.service.ExpressService;
-import com.express.service.ExpressShelfService;
-import com.express.service.OverDueExpressService;
-import com.express.service.SendMailService;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/express")
@@ -35,8 +25,6 @@ public class ExpressController {
 	ExpressService expressService;
 	@Autowired
 	ExpressShelfService expressShelfService;
-	@Resource
-	ExpressShelfDao expressShelfDao;
 	@Autowired
 	OverDueExpressService overDueExpressService;
 	@Autowired
@@ -47,17 +35,6 @@ public class ExpressController {
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String toIndex(Model model) {
 		return "express/express";
-	}
-
-	@ResponseBody
-	@RequestMapping(value = "/getShelf", method = RequestMethod.GET)
-	public Object getExpressShelf(HttpServletRequest request, HttpServletResponse response) {
-		Long id = Long.parseLong(request.getParameter("id"));
-		Express express = expressService.getExpressInfoById(id);
-		ExpressShelf shelf = new ExpressShelf();
-		shelf.setExpress(express);
-		shelf = expressShelfDao.queryShelfByParams(shelf);
-		return shelf;
 	}
 
 	@RequestMapping(value = "/vegas", method = RequestMethod.GET)
@@ -86,49 +63,49 @@ public class ExpressController {
 		express = expressService.queryExpressDetail(express);
 		if(expressService.affirmCode(express, postCode)){ // 判断是否通过验证
 			switch (status) { // 根据
-			case "O":
-				// 过期快件处理
-				// 获取OverDueExpress
-				// 根据返回的OverDueExpress对象
-				OverDueExpress overDueExpress = new OverDueExpress();
-				overDueExpress.setExpress(express);
-				overDueExpress = overDueExpressService.queryShelfByParams(overDueExpress);
-				// 删除对应的overDueExpress记录并更新订单
-				overDueExpress.setStatus("S");
-				express.setStatus("S");
-				express.setReciveDate(new Date());
-				overDueExpressService.updateOverDueExpressShelf(overDueExpress);// 修改过期货柜表中的状态
-				expressService.updateExpress(express);// 修改订单中的相应
-				ExpressHistory expressHistory = new ExpressHistory();
-				expressHistory.setShelfId("OverDueShelf");
-				expressHistory.setExpress(express);
-				expressHistory.setCreateDate(new Date());
-				expressHistoryService.insertExpressHistory(expressHistory);// 插入历史记录
-				// 发送已收件消息
-				jsonObject.put("location", "OverDueShelf");
-				break;
-			case "E":
-				// 当天快件处理
-				ExpressShelf expressShelf = new ExpressShelf();
-				expressShelf.setExpress(express);
-				expressShelf = expressShelfService.queryShelfByParams(expressShelf);
-				expressShelf.setShelfStatus("S");
-				express.setStatus("S");
-				express.setReciveDate(new Date());
-				jsonObject.put("location", expressShelf.getShelfId());
-				ExpressHistory expressHistoryE = new ExpressHistory();
-				expressHistoryE.setShelfId(expressShelf.getShelfId().toString());
-				expressHistoryE.setExpress(express);
-				expressHistoryE.setCreateDate(new Date());
-				expressShelf.setExpress(null);
-				expressShelf.setShelfStatus("N");
-				expressShelfService.updateExpressShelf(expressShelf); // 修改货柜记录
-				expressService.updateExpress(express);// 修改订单记录
-				expressHistoryService.insertExpressHistory(expressHistoryE);// 插入历史记录
-				// 发送已收件消息
-				break;
-			default:
-				break;
+				case "O":
+					// 过期快件处理
+					// 获取OverDueExpress
+					// 根据返回的OverDueExpress对象
+					OverDueExpress overDueExpress = new OverDueExpress();
+					overDueExpress.setExpress(express);
+					overDueExpress = overDueExpressService.queryShelfByParams(overDueExpress);
+					// 删除对应的overDueExpress记录并更新订单
+					overDueExpress.setStatus("S");
+					express.setStatus("S");
+					express.setReciveDate(new Date());
+					overDueExpressService.updateOverDueExpressShelf(overDueExpress);// 修改过期货柜表中的状态
+					expressService.updateExpress(express);// 修改订单中的相应
+					ExpressHistory expressHistory = new ExpressHistory();
+					expressHistory.setShelfId("OverDueShelf");
+					expressHistory.setExpress(express);
+					expressHistory.setCreateDate(new Date());
+					expressHistoryService.insertExpressHistory(expressHistory);// 插入历史记录
+					// 发送已收件消息
+					jsonObject.put("location", "OverDueShelf");
+					break;
+				case "E":
+					// 当天快件处理
+					ExpressShelf expressShelf = new ExpressShelf();
+					expressShelf.setExpress(express);
+					expressShelf = expressShelfService.queryShelfByParams(expressShelf);
+					expressShelf.setShelfStatus("S");
+					express.setStatus("S");
+					express.setReciveDate(new Date());
+					jsonObject.put("location", expressShelf.getShelfId());
+					ExpressHistory expressHistoryE = new ExpressHistory();
+					expressHistoryE.setShelfId(expressShelf.getShelfId().toString());
+					expressHistoryE.setExpress(express);
+					expressHistoryE.setCreateDate(new Date());
+					expressShelf.setExpress(null);
+					expressShelf.setShelfStatus("N");
+					expressShelfService.updateExpressShelf(expressShelf); // 修改货柜记录
+					expressService.updateExpress(express);// 修改订单记录
+					expressHistoryService.insertExpressHistory(expressHistoryE);// 插入历史记录
+					// 发送已收件消息
+					break;
+				default:
+					break;
 			}
 			sendMailService.sendGetExpressSuccessEmail(express);
 		}
