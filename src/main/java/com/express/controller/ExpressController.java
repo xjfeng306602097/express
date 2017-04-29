b0539cab6afef04a1cdfe035149a94d75135655f
wshibiao <790207553@qq.com>
2017-04-29 23:35:39 +0800

    迷の提交

--- src/main/java/com/express/controller/ExpressController.java
+++ src/main/java/com/express/controller/ExpressController.java
@@ -1,13 +1,14 @@
 package com.express.controller;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.alibaba.fastjson.JSONObject;
+import com.express.dao.ExpressShelfDao;
+import com.express.model.Express;
+import com.express.model.ExpressHistory;
+import com.express.model.ExpressShelf;
+import com.express.model.OverDueExpress;
+import com.express.service.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,17 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSONObject;
-import com.express.dao.ExpressShelfDao;
-import com.express.model.Express;
-import com.express.model.ExpressHistory;
-import com.express.model.ExpressShelf;
-import com.express.model.OverDueExpress;
-import com.express.service.ExpressHistoryService;
-import com.express.service.ExpressService;
-import com.express.service.ExpressShelfService;
-import com.express.service.OverDueExpressService;
-import com.express.service.SendMailService;
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/express")
@@ -43,6 +37,8 @@ public class ExpressController {
 	ExpressHistoryService expressHistoryService;
 	@Autowired
 	SendMailService sendMailService;
+	private static final Logger logger = (Logger) LoggerFactory.getLogger(ExpressController.class);
+
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String toIndex(Model model) {
