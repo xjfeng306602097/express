82e68d990bd38e6964a169640c93111537a6e10b
xjfeng306602097 <306602097@qq.com>
2017-05-03 12:16:40 +0800

    登录功能实现

--- WebContent/WEB-INF/js/admin.js
+++ WebContent/WEB-INF/js/admin.js
@@ -1,29 +1,36 @@
 /**
  * Created by wshibiao on 2017/4/19.
  */
-var Admin={
-    login:function () {
-        var user= {
-            userId: $("#userid" +
-                "").val(),
-            password: $("#password").val()
-        };
-        $.ajax({
-            type : "POST",
-            url : "login",
-            contentType : "application/json;chartset=utf-8",
-            data : JSON.stringify(user),
-            success : function(data) {
-                    window.location.href = "redirect";
-            },
-            error:function (data) {
-                alert(JSON.stringify(data));
-                window.location.href = "sap.html";
-            }
-            }
-        ,0);
-    },
-    logout:function () {
-        
-    }
+var Admin = {
+	login : function(user, vue) {
+		if (Admin.checkValidate(user)) {
+			vue.$http.post('login',user).then(function (response) {
+				if (response.data.loginStatus == 'success'){
+					window.location.href = "redirect";
+				} else {
+					alert(response.data.message);
+				}
+            }).catch(function (response) {
+            });
+		}
+	},
+	checkValidate : function(user) {
+		var userPatrn = /^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){4,19}$/; // 只能输入5-20个以字母开头、可带数字、“_”、“.”的字符串
+		if (user.userId && userPatrn.test(user.userId)) {
+			return true;
+		} else {
+			alert("只能输入5-20个以字母开头、可带数字、“_”、“.”的字符串");
+			return false;
+		}
+		var passwordPatrn = /^(\w){6,20}$/;
+		if (user.password && userPatrn.test(user.password)) {
+			return true;
+		} else {
+			alert("只能输入6-20个字母、数字、下划线的密码");
+			return false;
+		}
+	},
+	logout : function() {
+
+	}
 };
---
+++ src/test/java/com/express/test/UserTest.java
@@ -0,0 +1,39 @@
+package com.express.test;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+
+import com.express.model.User;
+import com.express.service.UserService;
+import com.express.util.PropertyUtil;
+
+public class UserTest extends BaseTestCase {
+	@Autowired
+	private UserService userService;
+
+	@Test
+	public void test() throws IOException {
+		User user = new User();
+		user.setUserId("a306602097");
+		user.setUserName("老肖");
+		user.setPassword(DigestUtils
+				.md5DigestAsHex(("a123456" + PropertyUtil.getProperty("Salt")).getBytes()));
+		userService.createUser(user);
+	}
+	
+	@Test
+	public void testPassword() throws IOException {
+		User params = new User();
+		params.setUserId("a306602097");
+		params.setPassword("a123456");
+		String userId = params.getUserId();
+		String password = DigestUtils
+				.md5DigestAsHex((params.getPassword() + PropertyUtil.getProperty("Salt")).getBytes());
+		User result = userService.getUserById(userId);
+		System.out.println(password.equals(result.getPassword()));
+	}
+
+}
